8c010fa28a406fdfb79931ba62f4df36aa5a32fb
Abhishek83gupta <abhi12345678gupta@gmail.com>
2024-10-08 21:16:18 +0530

    fix : minor problems

--- server/middlewares/auth.middleware.js
+++ server/middlewares/auth.middleware.js
@@ -1,20 +1,26 @@
 const jwt = require("jsonwebtoken");
 
 const authenticateToken = async (req, res, next) => {
-  const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1];
-
-  // No token , unauthorized
-  if(!token)
-    return res.sendStatus(401);
-
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err,user) => {
-    // Token invalid, forbidden
-    if(err) 
-    return res.sendStatus(401);
-    req.user = user;
-    next();
-  })
+  try {
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
+  
+    // No token , unauthorized
+    if(!token)
+      return res.sendStatus(401);
+  
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err,user) => {
+      // Token invalid, forbidden
+      if(err) 
+      return res.sendStatus(401);
+      req.user = user;
+      next();
+    })
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ success: false, message: error.message });
+  }
 }
 
-modelue.exports = authenticateToken;
+module.exports = authenticateToken;
