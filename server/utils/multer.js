2d7a0aea1cf45d6a0ce60e0627d6f39bb44dfd19
Abhishek83gupta <abhi12345678gupta@gmail.com>
2024-10-11 14:52:14 +0530

    feat : Image upload

---
+++ server/utils/multer.js
@@ -0,0 +1,25 @@
+const multer = require("multer");
+const path = require("path");
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "./uploads/"); // Destination folder for storing uploaded files
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + path.extname(file.originalname)); // Unique filename
+    },
+});
+
+// File filter to accept only images
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype.startsWith("image/")) {
+        cb(null, true); // Accept the file
+    } else {
+        cb(new Error("Only images are allowed"), false); // Reject the file
+    }
+};
+
+// Initialize multer instance
+const upload = multer({ storage, fileFilter });
+
+module.exports = upload;
