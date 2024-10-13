ad2753f77ae9386a441d414593592cae575ee2ec
Abhishek83gupta <abhi12345678gupta@gmail.com>
2024-10-13 18:14:59 +0530

    feat: done the routing, other is pending

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,45 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Layout from './Layout.jsx';
+import Login from './pages/Auth/Login.jsx';
+import Signup from './pages/Auth/Signup.jsx';
+import Home from './pages/Home/Home.jsx';
+import ProtectedRoute from './pages/Auth/ProtectedRoute.jsx'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import "react-day-picker/style.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout/>,
+    children:[
+      {
+       path: "/dashboard",
+       element: <Home/>
+       
+      },
+      {
+        path : "/login",
+        element : <Login/>
+      },
+      {
+        path: "/signup",
+        element: <Signup/>
+      }
+    ]
+  }
+])
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
+     <ToastContainer/>
   </StrictMode>,
 )
